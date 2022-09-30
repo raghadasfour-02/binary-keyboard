3df9e0b91ec4a4fc5071235d243aa1f048788917
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-09-30 12:27:05 -0400

    Configure Nav Bar

--- src/App.js
+++ src/App.js
@@ -106,6 +106,117 @@ function App() {
           </div>
         </nav>
 
+        <div className="relative" id="home">
+          <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
+          <div className="absolute bottom-10 w-full flex justify-center items-center">
+            <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
+              <p className="text-white">Press and drag to orbit</p>
+            </div>
+          </div>
+        </div>
+
+        <main className="w-[80%] mt-4">
+          <section
+            className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
+            id="about"
+          >
+            <div className="w-full h-420 flex items-center justify-center">
+              <div className="w-275 h-340 relative bg-emerald-200 rounded-md">
+                <img
+                  src={Vetri}
+                  alt=""
+                  className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl"
+                />
+              </div>
+            </div>
+            <div className="w-full h-420 flex flex-col items-center justify-center ">
+              <p className="text-lg text-textBase text-center">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Sapiente dicta recusandae commodi praesentium officia, aliquam
+                cumque voluptatem dolor enim ducimus id maiores consequatur
+                dolorem! Architecto aperiam corrupti nostrum eum similique!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis saepe dolor illo. Tenetur labore impedit nihil natus
+                explicabo perspiciatis nisi autem aut numquam dolorum, voluptas
+                vero nobis assumenda commodi magni.
+              </p>
+
+              <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+                <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Download
+                </span>
+              </button>
+            </div>
+          </section>
+
+          <section className="w-full flex items-center justify-center">
+            <VerticalTimeline>
+              {Experience &&
+                Experience.map((n) => (
+                  <VerticalTimelineElement
+                    key={n.id}
+                    className="vertical-timeline-element--work"
+                    contentStyle={{
+                      background: "rgb(21, 24, 31)",
+                      color: "#888",
+                    }}
+                    contentArrowStyle={{
+                      borderRight: "7px solid  rgb(21, 24, 31)",
+                    }}
+                    date={n.date}
+                    iconStyle={{ background: "rgb(21, 24, 31)", color: "#888" }}
+                    icon={n.iconsSrc}
+                  >
+                    <h3 className="vertical-timeline-element-title">
+                      {n.title}
+                    </h3>
+                    <h4 className="vertical-timeline-element-subtitle">
+                      {n.location}
+                    </h4>
+                    <p>{n.description}</p>
+                  </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
+          </section>
+
+          <section
+            className="flex flex-wrap items-center justify-evenly my-24 gap-4"
+            id="projects"
+          >
+            {Projects &&
+              Projects.map((n, i) => (
+                <motion.div
+                  key={n.id}
+                  className="border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[275px] hover:border-zinc-600 duration-100 ease-in-out"
+                >
+                  <p className="text-lg text-textBase font-medium uppercase">
+                    {n.name.length > 25 ? `${n.name.slice(0, 25)}...` : n.name}
+                  </p>
+
+                  <img
+                    src={n.imageSrc}
+                    className="w-full h-full object-cover rounded-md my-4"
+                    alt=""
+                  />
+
+                  <div className="flex flex-1 items-center justify-between">
+                    <p className="text-lg text-gray-300">
+                      Technologies
+                      <span className="block text-sm text-gray-500">
+                        {n.techs}
+                      </span>
+                    </p>
+                    <a href={n.github}>
+                      <motion.div whileTap={{ scale: 0.5 }}>
+                        <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />
+                      </motion.div>
+                    </a>
+                  </div>
+                </motion.div>
+              ))}
+          </section>
+        </main>
+      </div>
     </AnimatePresence>
   );
 }
