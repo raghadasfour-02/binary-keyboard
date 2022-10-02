7bd5d0e0177e798e1bab5c1d764c6c7f8df1a221
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-10-02 00:21:22 -0400

    Configure Spline Keyboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
-    <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+    <div className="flex min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
       <div className="relative" id="home">
         <Spline scene="https://prod.spline.design/qiwG55ZtideqQuZo/scene.splinecode" />
       </div>
